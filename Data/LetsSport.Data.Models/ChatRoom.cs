f28e1fa592f25b3d3387f5e9e4c77e1c9bb6782d
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 19:19:20 +0200

    Add ChatRoom DbModel

---
+++ Data/LetsSport.Data.Models/ChatRoom.cs
@@ -0,0 +1,17 @@
+﻿namespace LetsSport.Data.Models
+{
+    using System.Collections.Generic;
+
+    using LetsSport.Data.Common.Models;
+
+    public class ChatRoom : BaseDeletableModel<string>
+    {
+        public virtual Event Event { get; set; }
+
+        public int EventId { get; set; }
+
+        public virtual ICollection<User> Users { get; set; } = new HashSet<User>();
+
+        public virtual Stack<Message> Messages { get; set; } = new Stack<Message>();
+    }
+}
