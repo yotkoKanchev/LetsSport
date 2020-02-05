d5873dbd56246c3bb7bc63e4653f944d5d1bb8ab
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 11:08:29 +0200

    Update Country DbModel

--- Data/LetsSport.Data.Models/Country.cs
+++ Data/LetsSport.Data.Models/Country.cs
@@ -1,9 +1,7 @@
 ﻿namespace LetsSport.Data.Models
 {
-    using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    using System.Text;
 
     using LetsSport.Data.Common.Models;
 
