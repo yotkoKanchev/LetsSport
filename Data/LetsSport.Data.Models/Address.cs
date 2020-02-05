0e3820c84e45bd26b1aa1b5587fc8762647c54d9
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 11:08:10 +0200

    Add Address DbModel

---
+++ Data/LetsSport.Data.Models/Address.cs
@@ -0,0 +1,26 @@
+﻿namespace LetsSport.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsSport.Data.Common.Models;
+
+    public class Address : BaseDeletableModel<int>
+    {
+        [Required]
+        public Country Country { get; set; }
+
+        public int CountryId { get; set; }
+
+        [Required]
+        public City City { get; set; }
+
+        public int CityId { get; set; }
+
+        public Neighborhood Neighborhood { get; set; }
+
+        public int NeighborhoodId { get; set; }
+
+        [MaxLength(100)]
+        public string StreetAddress { get; set; }
+    }
+}
