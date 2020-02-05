4001b484f56b8d303a13c4f411a52e64c4f7cbbe
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 10:42:46 +0200

    Add City DbModel

---
+++ Data/LetsSport.Data.Models/City.cs
@@ -0,0 +1,26 @@
+﻿namespace LetsSport.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    using LetsSport.Data.Common.Models;
+
+    public class City : BaseDeletableModel<int>
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(10)]
+        public string ZipCode { get; set; }
+
+        [Required]
+        public Country Country { get; set; }
+
+        public int CountryId { get; set; }
+
+        // public ICollection<Neighborhood>() Neighborhoods { get; set; } = new HashSet<Neighborhood>();
+    }
+}
