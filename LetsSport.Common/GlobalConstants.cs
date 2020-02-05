098ce7fed5350e36ec88ac6148cc129806742151
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 19:16:28 +0200

    Add MaxNameLength constant

--- LetsSport.Common/GlobalConstants.cs
+++ LetsSport.Common/GlobalConstants.cs
@@ -5,5 +5,7 @@
         public const string SystemName = "LetsSport";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const int MaximumUserNameLength = 30;
     }
 }
