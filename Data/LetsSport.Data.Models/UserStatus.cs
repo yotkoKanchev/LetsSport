2a0bed5118d95b5e05bea1cfb25379e8546d0f31
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 18:35:51 +0200

    Add UserStatus enumeration

---
+++ Data/LetsSport.Data.Models/UserStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetsSport.Data.Models
+{
+    public enum UserStatus
+    {
+        Unknown = 0,
+        AwaysAvailable = 1,
+        Choosable = 2,
+        Unavailable = 3,
+    }
+}
