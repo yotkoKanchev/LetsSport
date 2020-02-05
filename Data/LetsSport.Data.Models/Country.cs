eca4f989d20cadc7ee9d44a2567e9dd0f9c0d317
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 10:38:36 +0200

    Add Country DbModel

---
+++ Data/LetsSport.Data.Models/Country.cs
@@ -0,0 +1,19 @@
+﻿namespace LetsSport.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
+    using LetsSport.Data.Common.Models;
+
+    public class Country : BaseDeletableModel<int>
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(3)]
+        public string CountryCode { get; set; }
+    }
+}
