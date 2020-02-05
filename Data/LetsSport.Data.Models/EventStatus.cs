84a26a48af14952192e2091e7ff48f708b66857c
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 14:33:56 +0200

    Add EventStatus enumeration

--- Data/LetsSport.Data.Models/EventStatus.cs
+++ Data/LetsSport.Data.Models/EventStatus.cs
@@ -4,9 +4,10 @@
     {
         None = 0,
         AcceptingPlayers = 1,
-        Full = 2,
-        InProgress = 3,
-        Finished = 4,
-        Failed = 5,
+        MinimumPlayersReached = 2,
+        Full = 3,
+        InProgress = 4,
+        Finished = 5,
+        Failed = 6,
     }
 }
