70dbb823bfeefc3fc3932197e67fe80cbde12ee9
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 19:18:53 +0200

    Add ArenaAdmin DbModel

---
+++ Data/LetsSport.Data.Models/ArenaAdmin.cs
@@ -0,0 +1,37 @@
+﻿namespace LetsSport.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsSport.Data.Common.Models;
+
+    using static LetsSport.Common.GlobalConstants;
+
+    public class ArenaAdmin : BaseDeletableModel<string>
+    {
+        [Required]
+        [MaxLength(MaximumUserNameLength)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(MaximumUserNameLength)]
+        public string LasstName { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Username { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Password { get; set; }
+
+        [MaxLength(100)]
+        public string Occupation { get; set; }
+
+        [MaxLength(100)]
+        public string PhoneNumber { get; set; }
+    }
+}
