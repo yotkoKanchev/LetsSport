9a60cf03e9e6916c1501a4e2b36980572c1dd7d6
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 19:17:59 +0200

    Update UserStatus

--- Data/LetsSport.Data.Models/UserStatus.cs
+++ Data/LetsSport.Data.Models/UserStatus.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LetsSport.Data.Models
+﻿namespace LetsSport.Data.Models
 {
     public enum UserStatus
     {
         Unknown = 0,
-        AwaysAvailable = 1,
-        Choosable = 2,
+        ProposalOpen = 1,
+        ProposalClosed = 2,
         Unavailable = 3,
     }
 }
