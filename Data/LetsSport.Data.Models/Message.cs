e0a947804c90a83a84d2949600e0640ade707498
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-05 19:18:30 +0200

    Add Message DbModel

---
+++ Data/LetsSport.Data.Models/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace LetsSport.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsSport.Data.Common.Models;
+
+    public class Message : BaseDeletableModel<string>
+    {
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public virtual User User { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
