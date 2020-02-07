0352b3c87c0babef92172e3b5116c8ac8161d402
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-07 11:52:37 +0200

    Add navigation property to Arena DbModel

--- Data/LetsSport.Data.Models/AddressModels/Address.cs
+++ Data/LetsSport.Data.Models/AddressModels/Address.cs
@@ -3,18 +3,21 @@
     using System.ComponentModel.DataAnnotations;
 
     using LetsSport.Data.Common.Models;
+    using LetsSport.Data.Models.ArenaModels;
 
     public class Address : BaseDeletableModel<int>
     {
-        public int CityId { get; set; }
-
-        [Required]
-        public virtual City City { get; set; }
-
         [MaxLength(10)]
         public string ZipCode { get; set; }
 
         [MaxLength(100)]
         public string StreetAddress { get; set; }
+
+        public int CityId { get; set; }
+
+        [Required]
+        public virtual City City { get; set; }
+
+        public Arena Arena { get; set; }
     }
 }
