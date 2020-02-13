1585a781f7b6ec492771580ed029878b0425e07c
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-13 16:38:16 +0200

    Udate EventUser DbModel

---
+++ Data/LetsSport.Data.Models/Mappings/EventUser.cs
@@ -0,0 +1,21 @@
+﻿namespace LetsSport.Data.Models.Mappings
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsSport.Data.Models.EventModels;
+    using LetsSport.Data.Models.UserModels;
+
+    public class EventUser
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+
+        [Required]
+        public int EventId { get; set; }
+
+        public virtual Event Event { get; set; }
+    }
+}
