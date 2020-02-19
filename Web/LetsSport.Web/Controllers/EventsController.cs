8863e96a1745bac18a22b32ed86df057bbf75789
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-19 10:24:24 +0200

    Add Create Action in EventsController

--- Web/LetsSport.Web/Controllers/EventsController.cs
+++ Web/LetsSport.Web/Controllers/EventsController.cs
@@ -1,6 +1,13 @@
 ﻿namespace LetsSport.Web.Controllers
 {
+
+    using Microsoft.AspNetCore.Mvc;
+
     public class EventsController : BaseController
     {
+        public IActionResult Create()
+        {
+            return this.View();
+        }
     }
 }
