9acc1d75feff5f7051784966ed48f05200199833
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-19 11:34:35 +0200

    Add new migration

---
+++ Data/LetsSport.Data/Migrations/20200219081006_AddDateToEventModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LetsSport.Data.Migrations
+{
+    public partial class AddDateToEventModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndingHour",
+                table: "Events");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Events",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Events");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndingHour",
+                table: "Events",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
---
+++ Data/LetsSport.Data/Migrations/20200219093037_RemoveRatingFromArenaModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LetsSport.Data.Migrations
+{
+    public partial class RemoveRatingFromArenaModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Arenas");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Sport",
+                table: "Arenas",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sport",
+                table: "Arenas");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Rating",
+                table: "Arenas",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
