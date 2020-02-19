cbc5717ef0c717d810aa28a813541d745bd5fd84
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-20 00:04:14 +0200

    Update EventsController

--- Web/LetsSport.Web/Controllers/EventsController.cs
+++ Web/LetsSport.Web/Controllers/EventsController.cs
@@ -1,11 +1,22 @@
 ﻿namespace LetsSport.Web.Controllers
 {
+    using LetsSport.Services.Data;
+    using LetsSport.Web.ViewModels.Events;
     using Microsoft.AspNetCore.Mvc;
 
     public class EventsController : BaseController
     {
         public IActionResult Create()
         {
+            // TODO pass filtered by sport Arenas with AJAX;
+            //var arenas = this.arenasService.GetAll();
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EventCreateInputModel inputModel)
+        {
+            var model = inputModel;
             // TODO pass filtered by sport Arenas with AJAX;
             return this.View();
         }
