3dd7dbd17b40fcb15bbdb62b65eae79a108b0528
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-19 11:32:06 +0200

    Add Create Action in ArenasController and EventsController

--- Web/LetsSport.Web/Controllers/ArenasController.cs
+++ Web/LetsSport.Web/Controllers/ArenasController.cs
@@ -1,6 +1,15 @@
 ﻿namespace LetsSport.Web.Controllers
 {
+    using LetsSport.Data.Models.AddressModels;
+    using Microsoft.AspNetCore.Mvc;
+
     public class ArenasController : BaseController
     {
+        public IActionResult Create()
+        {
+            // TODO pass filtered cities per country
+            // TODO add current country as default
+            return this.View();
+        }
     }
 }
--- Web/LetsSport.Web/Controllers/EventsController.cs
+++ Web/LetsSport.Web/Controllers/EventsController.cs
@@ -1,12 +1,12 @@
 ﻿namespace LetsSport.Web.Controllers
 {
-
     using Microsoft.AspNetCore.Mvc;
 
     public class EventsController : BaseController
     {
         public IActionResult Create()
         {
+            // TODO pass filtered by sport Arenas with AJAX;
             return this.View();
         }
     }
