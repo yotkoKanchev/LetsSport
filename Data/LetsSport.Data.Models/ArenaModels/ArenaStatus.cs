605847ca46502e379868e4c33810e91ad9ce6ada
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-19 11:33:36 +0200

    Add Arena Status Enumeration

---
+++ Data/LetsSport.Data.Models/ArenaModels/ArenaStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace LetsSport.Data.Models.ArenaModels
+{
+    public enum ArenaStatus
+    {
+        Active = 1,
+        InActive = 2,
+        InReconstruction = 3,
+        TemporatyNotAvailable = 4,
+        OpeningSoon = 5,
+        Closed = 6,
+    }
+}
