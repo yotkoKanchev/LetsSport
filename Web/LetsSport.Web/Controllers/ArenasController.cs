0931a4934f4a1053bc9858277c509742f67fc6c8
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-20 00:03:48 +0200

    Add ArenaS Create Method and update View

--- Web/LetsSport.Web/Controllers/ArenasController.cs
+++ Web/LetsSport.Web/Controllers/ArenasController.cs
@@ -1,15 +1,41 @@
 ﻿namespace LetsSport.Web.Controllers
 {
-    using LetsSport.Data.Models.AddressModels;
+    using LetsSport.Services.Data;
+    using LetsSport.Web.ViewModels.Arenas;
     using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
 
     public class ArenasController : BaseController
     {
+        private readonly IArenasService arenasService;
+        private readonly IAddressesService addressesService;
+
+        public ArenasController(IArenasService arenasService, IAddressesService addressesService)
+        {
+            this.arenasService = arenasService;
+            this.addressesService = addressesService;
+        }
+
         public IActionResult Create()
         {
             // TODO pass filtered cities per country
             // TODO add current country as default
+            var cities = this.addressesService.GetCities();
+            this.ViewData["cities"] = cities;
             return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ArenaCreateInputModel inputModel)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View("Error");
+            }
+
+            await this.arenasService.Create(inputModel);
+            // TODO pass filtered by sport Arenas with AJAX;
+            return this.Redirect("/");
         }
     }
 }
