d7306ae5f5cfed2e62924321c918b0eea1b42b36
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-20 00:03:06 +0200

    Register IArenasService and IAddressesService

--- Web/LetsSport.Web/Startup.cs
+++ Web/LetsSport.Web/Startup.cs
@@ -58,6 +58,8 @@
             // Application services
             services.AddTransient<IEmailSender, NullMessageSender>();
             services.AddTransient<ISettingsService, SettingsService>();
+            services.AddTransient<IArenasService, ArenasService>();
+            services.AddTransient<IAddressesService, AddressesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
