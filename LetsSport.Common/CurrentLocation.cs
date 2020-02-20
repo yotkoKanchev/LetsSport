efe5166eb747c8d751b55ad8020e66e83e7a4ebe
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-20 10:47:22 +0200

    Update CurrentLocation class to return Tuple of City and Country names

--- LetsSport.Common/CurrentLocation.cs
+++ LetsSport.Common/CurrentLocation.cs
@@ -7,33 +7,18 @@
 
     public static class CurrentLocation
     {
-        public static string GetCurrentCity()
-        {
-            string info = new WebClient().DownloadString("http://ipinfo.io/");
-            var ipInfo = JsonConvert.DeserializeObject<IpInfo>(info);
-            var city = ipInfo.City;
-
-            return city;
-        }
-
-        public static string GetCountry()
+        public static (string Country, string City) GetLocationInfo()
         {
             string info = new WebClient().DownloadString("http://ipinfo.io/");
             var ipInfo = JsonConvert.DeserializeObject<IpInfo>(info);
             RegionInfo countryInfo = new RegionInfo(ipInfo.Country);
-            var country = countryInfo.EnglishName;
-
-            return country;
+            var countryName = countryInfo.EnglishName;
+            var cityName = ipInfo.City;
+            return (countryName, cityName);
         }
 
         private class IpInfo
         {
-            [JsonProperty("ip")]
-            public string Ip { get; set; }
-
-            [JsonProperty("hostname")]
-            public string Hostname { get; set; }
-
             [JsonProperty("city")]
             public string City { get; set; }
 
@@ -42,12 +27,6 @@
 
             [JsonProperty("country")]
             public string Country { get; set; }
-
-            [JsonProperty("loc")]
-            public string Loc { get; set; }
-
-            [JsonProperty("org")]
-            public string Org { get; set; }
 
             [JsonProperty("postal")]
             public string Postal { get; set; }
