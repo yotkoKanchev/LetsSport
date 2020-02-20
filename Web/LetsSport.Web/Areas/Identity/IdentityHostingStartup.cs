ba6c467c4299706ae1c88f997f051196d01e315a
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-20 14:35:37 +0200

    Scaffold Identity

---
+++ Web/LetsSport.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LetsSport.Data;
+using LetsSport.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LetsSport.Web.Areas.Identity.IdentityHostingStartup))]
+namespace LetsSport.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
