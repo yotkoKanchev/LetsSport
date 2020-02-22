87a15d2eb6cdace4b350338ad0bba21c3f0fbdd1
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-22 16:54:56 +0200

    Update Arena Edit GET

---
+++ Web/LetsSport.Web.ViewModels/Arenas/ArenaEditViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LetsSport.Web.ViewModels.Arenas
+{
+    public class ArenaEditViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SportType { get; set; }
+
+        public double PricePerHour { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string WebUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string Address { get; set; }
+    }
+}
