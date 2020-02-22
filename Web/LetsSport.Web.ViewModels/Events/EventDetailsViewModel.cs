711cbe7b3ed049a9d595655d68a6e8ac9a63aeed
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-22 19:01:48 +0200

    Update property naming with Display attribute

--- Web/LetsSport.Web.ViewModels/Events/EventDetailsViewModel.cs
+++ Web/LetsSport.Web.ViewModels/Events/EventDetailsViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace LetsSport.Web.ViewModels.Events
+﻿using System.ComponentModel;
+
+namespace LetsSport.Web.ViewModels.Events
 {
     public class EventDetailsViewModel
     {
@@ -6,40 +8,52 @@
 
         public string Name { get; set; }
 
+        [DisplayName("Sport Type")]
         public string SportType { get; set; }
 
         public string Arena { get; set; }
 
         public string Date { get; set; }
 
+        [DisplayName("Start time")]
         public string StartingHour { get; set; }
 
         public string Admin { get; set; }
 
+        [DisplayName("Game format")]
         public string GameFormat { get; set; }
 
         public string Gender { get; set; }
 
+        [DisplayName("Duration in hours")]
         public double DurationInHours { get; set; }
 
+        [DisplayName("Total price")]
         public double TotalPrice { get; set; }
 
+        [DisplayName("Minimum players")]
         public int MinPlayers { get; set; }
 
+        [DisplayName("Maximum players")]
         public int MaxPlayers { get; set; }
 
+        [DisplayName("Empty spots")]
         public int EmptySpotsLeft { get; set; }
 
+        [DisplayName("Needed Players")]
         public int NeededPlayersForConfirmation { get; set; }
 
+        [DisplayName("Request Deadline")]
         public string DeadLineToSendRequest { get; set; }
 
+        [DisplayName("Additional info")]
         public string AdditionalInfo { get; set; }
 
         public string Players { get; set; }
 
         public string Status { get; set; }
 
+        [DisplayName("Request Status")]
         public string RequestStatus { get; set; }
     }
 }
