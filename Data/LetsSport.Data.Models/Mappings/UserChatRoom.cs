292fd391e7765c178935804fe0b45d4138585486
yotkoKanchev <yotko.kanchev@gmail.com>
2020-02-25 11:30:54 +0200

    Add UserChatRoom mapping table

---
+++ Data/LetsSport.Data.Models/Mappings/UserChatRoom.cs
@@ -0,0 +1,19 @@
+﻿namespace LetsSport.Data.Models.Mappings
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using LetsSport.Data.Models.ChatModels;
+
+    public class UserChatRoom
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        [Required]
+        public string ChatRoomId { get; set; }
+
+        public ChatRoom ChatRoom { get; set; }
+    }
+}
