49a5df349fd00d8e4ed2db7956564fba9b818a2e
yotkoKanchev <yotko.kanchev@gmail.com>
2020-03-02 09:58:31 +0200

    Scaffold Identity/Account/Manage

---
+++ Services/LetsSport.Services.Data/IUsersService.cs
@@ -0,0 +1,11 @@
+﻿namespace LetsSport.Services.Data
+{
+    using System.Collections.Generic;
+
+    using LetsSport.Data.Models.EventModels;
+
+    public interface IUsersService
+    {
+        IList<Event> GetUserEvents(string userId);
+    }
+}
---
+++ Services/LetsSport.Services.Data/UsersService.cs
@@ -0,0 +1,33 @@
+﻿namespace LetsSport.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using LetsSport.Data.Common.Repositories;
+    using LetsSport.Data.Models;
+    using LetsSport.Data.Models.EventModels;
+
+    public class UsersService : IUsersService
+    {
+        private readonly IDeletableEntityRepository<ApplicationUser> usersRepository;
+        private readonly IRepository<Event> eventsRepository;
+
+        public UsersService(IDeletableEntityRepository<ApplicationUser> usersRepository, IRepository<Event> eventsRepository)
+        {
+            this.usersRepository = usersRepository;
+            this.eventsRepository = eventsRepository;
+        }
+
+        public IList<Event> GetUserEvents(string userId)
+        {
+            var events = this.usersRepository
+                .AllAsNoTracking()
+                .Where(u => u.Id == userId)
+                .Select(u => u.Events
+                    .Select(ue => ue.Event)
+                    .ToList())
+                .FirstOrDefault();
+
+            return events;
+        }
+    }
+}
