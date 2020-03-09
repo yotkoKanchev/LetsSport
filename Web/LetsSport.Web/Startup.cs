e1e6b875c7fd9427e56f3857fd0fae10858d893c
yotkoKanchev <yotko.kanchev@gmail.com>
2020-03-09 14:05:12 +0200

    register SendGrid account

--- Web/LetsSport.Web/Startup.cs
+++ Web/LetsSport.Web/Startup.cs
@@ -59,7 +59,7 @@
             services.AddScoped<IDbQueryRunner, DbQueryRunner>();
 
             // Application services
-            services.AddTransient<IEmailSender, NullMessageSender>();
+            services.AddTransient<IEmailSender>(x => new SendGridEmailSender(this.configuration["SendGrid:AppKey"]));
             services.AddTransient<ISettingsService, SettingsService>();
             services.AddTransient<IArenasService, ArenasService>();
             services.AddTransient<IEventsService, EventsService>();
