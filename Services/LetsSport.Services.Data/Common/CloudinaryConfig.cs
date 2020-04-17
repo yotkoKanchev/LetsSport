d7998726641d75a3aead6687e3f2994e5a54a00d
yotkoKanchev <yotko.kanchev@gmail.com>
2020-04-17 12:10:06 +0300

    update CloudinaryConfig

--- Services/LetsSport.Services.Data/Common/CloudinaryConfig.cs
+++ Services/LetsSport.Services.Data/Common/CloudinaryConfig.cs
@@ -2,8 +2,6 @@
 {
     public static class CloudinaryConfig
     {
-        //public const string ApiName = "dziee8jfp";
-        //public const string ApiKey = "359938615967177";
-        //public const string ApiSecret = "ygXea3zH9vkMiXvzsUPEs3zNTFA";
+
     }
 }
