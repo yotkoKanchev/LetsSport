1a425786c59ead45b87f9eb3a0034ba9ecf29781
yotkoKanchev <yotko.kanchev@gmail.com>
2020-09-21 12:34:54 +0300

    Update EventsSeeder

--- Web/LetsSport.Web/Startup.cs
+++ Web/LetsSport.Web/Startup.cs
@@ -41,7 +41,8 @@
                 options => options.UseSqlServer(this.configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDefaultIdentity<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions)
-                .AddRoles<ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddRoles<ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.Configure<CookiePolicyOptions>(
                 options =>
